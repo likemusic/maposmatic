9794546c9ee3aa94c62dbca409525a586b985149
Hartmut Holzgraefe <hartmut@php.net>
2016-01-15 15:54:32 +0000

    quick workaround around setup problem

--- www/settings.py
+++ www/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'tm+wb)lp5q%br=p0d2toz&km_-w)cmcelv!7inons&^v9(q!d2'
