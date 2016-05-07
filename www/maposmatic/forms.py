364473d293cbf4a6f06121920c44657f834efc45
Hartmut Holzgraefe <hartmut@php.net>
2016-05-07 13:29:03 +0200

    added explicit "no overlay" option which is active by default

--- www/maposmatic/forms.py
+++ www/maposmatic/forms.py
@@ -65,7 +65,7 @@ class MapRenderingJobForm(forms.ModelForm):
                              widget=forms.RadioSelect)
     layout = forms.ChoiceField(choices=(), widget=forms.RadioSelect)
     stylesheet = forms.ChoiceField(choices=(), widget=forms.RadioSelect)
-    overlay = forms.ChoiceField(choices=(), widget=forms.RadioSelect)
+    overlay = forms.ChoiceField(choices=(), widget=forms.RadioSelect, required=False)
     papersize = forms.ChoiceField(choices=(), widget=forms.RadioSelect)
     paperorientation = forms.ChoiceField(choices=ORIENTATION,
                                          widget=forms.RadioSelect)
@@ -124,7 +124,10 @@ class MapRenderingJobForm(forms.ModelForm):
                 description = mark_safe(_("The <i>%(stylesheet_name)s</i> stylesheet") % {'stylesheet_name':s.name})
             self.fields['stylesheet'].choices.append((s.name, description))
 
+        self.fields['stylesheet'].initial = stylesheets[0].name
+
         self.fields['overlay'].choices = []
+        self.fields['overlay'].choices.append(('', _('no overlay')))
         for s in overlays:
             if s.description is not None:
                 description = mark_safe(s.description)
@@ -132,7 +135,7 @@ class MapRenderingJobForm(forms.ModelForm):
                 description = mark_safe(_("The <i>%(stylesheet_name)s</i> overlay") % {'stylesheet_name':s.name})
             self.fields['overlay'].choices.append((s.name, description))
 
-        self.fields['stylesheet'].initial = stylesheets[0].name
+        self.fields['overlay'].initial = ''
 
         def _build_papersize_description(p):
             if p[0] == "Best fit":
@@ -271,7 +274,6 @@ class MapPaperSizeForm(forms.Form):
     osmid            = forms.IntegerField(required=False)
     layout           = forms.CharField(max_length=256)
     stylesheet       = forms.CharField(max_length=256)
-    overlay          = forms.CharField(max_length=256)
     lat_upper_left   = forms.FloatField(required=False, min_value=-90.0, max_value=90.0)
     lon_upper_left   = forms.FloatField(required=False, min_value=-180.0, max_value=180.0)
     lat_bottom_right = forms.FloatField(required=False, min_value=-90.0, max_value=90.0)
