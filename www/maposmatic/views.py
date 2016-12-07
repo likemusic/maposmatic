a93a7724f1f0bbe7fc1019aa68810a0dc7f3038f
Hartmut Holzgraefe <hartmut@php.net>
2016-12-07 01:29:12 +0100

    show true boundaries for city polygons in map detail page
    using leaflet omnivore plugin to render WKT

--- www/maposmatic/views.py
+++ www/maposmatic/views.py
@@ -312,7 +312,6 @@ def api_papersize(request):
     return HttpResponse(content=json_encode(paper_sizes),
                         content_type='text/json')
 
-
 def api_bbox(request, osm_id):
     """API handler that returns the bounding box from an OSM ID polygon."""
 
@@ -331,3 +330,23 @@ def api_bbox(request, osm_id):
         LOG.exception("Error calculating bounding box for OSM ID %d!" % osm_id)
 
     return HttpResponseBadRequest("ERROR: OSM ID %d not found!" % osm_id)
+
+def api_polygon(request, osm_id):
+    """API handler that returns the polygon outline from an OSM ID polygon."""
+
+    try:
+        osm_id = int(osm_id)
+    except ValueError:
+        return HttpResponseBadRequest("ERROR: Invalid arguments")
+
+    renderer = ocitysmap.OCitySMap(www.settings.OCITYSMAP_CFG_PATH)
+    try:
+        bbox_wkt, area_wkt = renderer.get_geographic_info(osm_id)
+        bbox = ocitysmap.coords.BoundingBox.parse_wkt(bbox_wkt).as_json_bounds()
+        return HttpResponse(content=json_encode({'bbox': bbox, 'wkt': area_wkt}),
+                            content_type='text/json')
+    except:
+        LOG.exception("Error retrieving polygon outline for OSM ID %d!" % osm_id)
+
+    return HttpResponseBadRequest("ERROR: OSM ID %d not found!" % osm_id)
+
