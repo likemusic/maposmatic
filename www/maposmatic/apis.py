c7bed20dbb33e0f9611b843fe00502e94cb23d22
Hartmut Holzgraefe <hartmut@php.net>
2018-09-10 21:54:05 +0000

    split up over-long jobs method

--- www/maposmatic/apis.py
+++ www/maposmatic/apis.py
@@ -27,7 +27,7 @@
 import json
 
 from django.core.exceptions import ValidationError
-from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse, HttpResponseNotAllowed, Http404
 from django.forms.models import model_to_dict
 from django.shortcuts import get_object_or_404
 
@@ -101,170 +101,182 @@ def jobs(request, job_id):
     """API handler for external rendering requests"""
 
     if request.method == 'GET':
-        if not job_id:
-            return HttpResponseNotFound('not found')
+        return _jobs_get(request, job_id)
+    elif request.method == 'POST':
+        return _jobs_post(request)
+    else:
+        return HttpResponseNotAllowed(['GET','POST'])
 
-        job = get_object_or_404(models.MapRenderingJob, id=job_id)
+def _jobs_get(request, job_id):
+    if not job_id:
+        return HttpResponseNotFound('not found')
 
-        reply = model_to_dict(job)
+    job = get_object_or_404(models.MapRenderingJob, id=job_id)
 
-        result = {}
-        result['id']              = job_id
-        result['status']          = reply['status']
+    reply = model_to_dict(job)
 
-        if reply['administrative_osmid']:
-            result['osm_id']      = reply['administrative_osmid']
+    result = {}
+    result['id']              = job_id
+    result['status']          = reply['status']
+
+    if reply['administrative_osmid']:
+        result['osm_id']      = reply['administrative_osmid']
+    else:
+        result['bbox_top']    = reply['lat_upper_left']
+        result['bbox_bottom'] = reply['lat_bottom_right']
+        result['bbox_left']   = reply['lon_upper_left']
+        result['bbox_right']  = reply['lon_bottom_right']
+
+    result['title']           = reply['maptitle']
+    result['layout']          = reply['layout']
+    result['style']           = reply['stylesheet']
+
+    if reply['overlay']:
+        result['overlays']    = reply['overlay'].split(',')
+
+    result['language']        = reply['map_language']
+    result['paper_height_mm'] = reply['paper_height_mm']
+    result['paper_width_mm']  = reply['paper_width_mm']
+
+    if job.status == 0:
+        result['queue_size'] = models.MapRenderingJob.objects.queue_size()
+    else:
+        result['queue_size'] = 0
+
+    result['files'] = {}
+    if job.status == 2:
+        files = job.output_files()
+        for key, val in files['maps'].items():
+            result['files'][key] = request.build_absolute_uri(val[0])
+
+    if job.status <= 1:
+        status = 202
+    else:
+        status = 200
+
+    return HttpResponse( content=json.dumps(result, indent=4, sort_keys=True, default=str)
+                       , content_type='text/json', status=status)
+
+
+
+def _jobs_post(request):
+    result = { "error": {} }
+
+    job = models.MapRenderingJob()
+
+    if request.content_type == 'application/json':
+        input = json.loads(request.body.decode('utf-8'))
+    else:
+        input = json.loads(request.POST['job'])
+
+    valid_keys = ['osmid', 'bbox_top', 'bbox_bottom', 'bbox_left', 'bbox_right',
+                  'title', 'language', 'layout', 'style', 'overlays',
+                  'paper_size', 'orientation']
+
+    for key in input:
+        if key not in valid_keys:
+            result['error'][key] = "Unknown parameter '%s'" % key
+
+    if 'osmid' in input:
+        job.administrative_osmid= input['osmid']
+    elif 'bbox_top' in input:
+        job.lat_upper_left   = input['bbox_top']
+        job.lon_upper_left   = input['bbox_left']
+        job.lat_bottom_right = input['bbox_bottom']
+        job.lon_bottom_right = input['bbox_right']
+    else:
+        result['error']['geometry'] = 'No bounding box or OsmID given'
+
+    if 'title' in input:
+        job.maptitle = input['title']
+
+    if 'language' in input:
+        job.map_language = input['language']
+    else:
+        job.map_language = 'en_US.UTF-8'
+
+    if 'layout' in input:
+        job.layout = input['layout']
+    else:
+        job.layout = 'plain'
+
+    if 'style' in input:
+        job.stylesheet = input['style']
+    else:
+        job.stylesheet = 'CartoOSM'
+
+    if 'overlays' in input:
+        if isinstance(input['overlays'], str):
+            job.overlay = input['overlays']
         else:
-            result['bbox_top']    = reply['lat_upper_left']
-            result['bbox_bottom'] = reply['lat_bottom_right']
-            result['bbox_left']   = reply['lon_upper_left']
-            result['bbox_right']  = reply['lon_bottom_right']
+            job.overlay = ",".join(input['overlays'])
 
-        result['title']           = reply['maptitle']
-        result['layout']          = reply['layout']
-        result['style']           = reply['stylesheet']
+    job.paper_width_mm  = 210
+    job.paper_height_mm = 297
 
-        if reply['overlay']:
-            result['overlays']    = reply['overlay'].split(',')
+    if 'paper_size' in input:
+        try:
+            p = ocitysmap.OCitySMap.get_paper_size_by_name(input['paper_size'])
+            if 'orientation' in input:
+                if input['orientation'] == 'landscape':
+                    p[1], p[0] = p[0], p[1]
+            job.paper_width_mm  = p[0]
+            job.paper_height_mm = p[1]
+        except LookupError as e:
+            result['error']['paper_size']  = str(e)
 
-        result['language']        = reply['map_language']
-        result['paper_height_mm'] = reply['paper_height_mm']
-        result['paper_width_mm']  = reply['paper_width_mm']
+    if not result['error']:
+        job.status = 0
+        job.submitterip = request.META['REMOTE_ADDR']
+        job.index_queue_at_submission = (models.MapRenderingJob.objects.queue_size())
+        job.nonce = helpers.generate_nonce(models.MapRenderingJob.NONCE_SIZE)
+        try:
+            job.full_clean()
+            job.save()
 
-        if job.status == 0:
-            result['queue_size'] = models.MapRenderingJob.objects.queue_size()
-        else:
-            result['queue_size'] = 0
+            if 'umap' in request.FILES:
+                job.umap.save('umap', request.FILES['umap'])
 
-        result['files'] = {}
-        if job.status == 2:
-            files = job.output_files()
-            for key, val in files['maps'].items():
-                result['files'][key] = request.build_absolute_uri(val[0])
+            if 'track' in request.FILES:
+                job.track.save('track', request.FILES['track'])
 
-        if job.status <= 1:
-            status = 202
-        else:
-            status = 200
+            if 'poi_file' in request.FILES:
+                job.poi_file.save('poi_file', request.FILES['poi_file'])
 
+            reply = model_to_dict(job)
 
-    if request.method == 'POST':
-        result = { "error": {} }
+            result['id']              = reply['id']
+            result['status']          = reply['status']
 
-        job = models.MapRenderingJob()
+            result['interactive']     = request.build_absolute_uri('../../maps/%d' % reply['id'])
 
-        if request.content_type == 'application/json':
-            input = json.loads(request.body.decode('utf-8'))
-        else:
-            input = json.loads(request.POST['job'])
-
-        valid_keys = ['osmid', 'bbox_top', 'bbox_bottom', 'bbox_left', 'bbox_right',
-                      'title', 'language', 'layout', 'style', 'overlays',
-                      'paper_size', 'orientation']
-
-        for key in input:
-            if key not in valid_keys:
-                result['error'][key] = "Unknown parameter '%s'" % key
-
-        if 'osmid' in input:
-            job.administrative_osmid= input['osmid']
-        elif 'bbox_top' in input:
-            job.lat_upper_left   = input['bbox_top']
-            job.lon_upper_left   = input['bbox_left']
-            job.lat_bottom_right = input['bbox_bottom']
-            job.lon_bottom_right = input['bbox_right']
-        else:
-            result['error']['geometry'] = 'No bounding box or OsmID given'
-
-        if 'title' in input:
-            job.maptitle = input['title']
-
-        if 'language' in input:
-            job.map_language = input['language']
-        else:
-            job.map_language = 'en_US.UTF-8'
-
-        if 'layout' in input:
-            job.layout = input['layout']
-        else:
-            job.layout = 'plain'
-
-        if 'style' in input:
-            job.stylesheet = input['style']
-        else:
-            job.stylesheet = 'CartoOSM'
-
-        if 'overlays' in input:
-            if isinstance(input['overlays'], str):
-                job.overlay = input['overlays']
+            if reply['administrative_osmid']:
+                result['osm_id']      = reply['administrative_osmid']
             else:
-                job.overlay = ",".join(input['overlays'])
+                result['bbox_top']    = reply['lat_upper_left']
+                result['bbox_bottom'] = reply['lat_bottom_right']
+                result['bbox_left']   = reply['lon_upper_left']
+                result['bbox_right']  = reply['lon_bottom_right']
 
-        job.paper_width_mm  = 210
-        job.paper_height_mm = 297
+            result['title']           = reply['maptitle']
+            result['layout']          = reply['layout']
+            result['style']           = reply['stylesheet']
+            if reply['overlay']:
+                result['overlays']    = reply['overlay'].split(',')
+            result['language']        = reply['map_language']
+            result['paper_height_mm'] = reply['paper_height_mm']
+            result['paper_width_mm']  = reply['paper_width_mm']
+        except ValidationError as e:
+            result['error'] = e.message_dict
+        except Exception as e:
+            result['error']['reason'] = str(e)
 
-        if 'paper_size' in input:
-            try:
-                p = ocitysmap.OCitySMap.get_paper_size_by_name(input['paper_size'])
-                if 'orientation' in input:
-                    if input['orientation'] == 'landscape':
-                        p[1], p[0] = p[0], p[1]
-                job.paper_width_mm  = p[0]
-                job.paper_height_mm = p[1]
-            except LookupError as e:
-                result['error']['paper_size']  = str(e)
-
-        if not result['error']:
-            job.status = 0
-            job.submitterip = request.META['REMOTE_ADDR']
-            job.index_queue_at_submission = (models.MapRenderingJob.objects.queue_size())
-            job.nonce = helpers.generate_nonce(models.MapRenderingJob.NONCE_SIZE)
-            try:
-                job.full_clean()
-                job.save()
-
-                if 'umap' in request.FILES:
-                    job.umap.save('umap', request.FILES['umap'])
-
-                if 'track' in request.FILES:
-                    job.track.save('track', request.FILES['track'])
-
-                if 'poi_file' in request.FILES:
-                    job.poi_file.save('poi_file', request.FILES['poi_file'])
-
-                reply = model_to_dict(job)
-
-                result['id']              = reply['id']
-                result['status']          = reply['status']
-
-                result['interactive']     = request.build_absolute_uri('../../maps/%d' % reply['id'])
-
-                if reply['administrative_osmid']:
-                    result['osm_id']      = reply['administrative_osmid']
-                else:
-                    result['bbox_top']    = reply['lat_upper_left']
-                    result['bbox_bottom'] = reply['lat_bottom_right']
-                    result['bbox_left']   = reply['lon_upper_left']
-                    result['bbox_right']  = reply['lon_bottom_right']
-
-                result['title']           = reply['maptitle']
-                result['layout']          = reply['layout']
-                result['style']           = reply['stylesheet']
-                if reply['overlay']:
-                    result['overlays']    = reply['overlay'].split(',')
-                result['language']        = reply['map_language']
-                result['paper_height_mm'] = reply['paper_height_mm']
-                result['paper_width_mm']  = reply['paper_width_mm']
-            except ValidationError as e:
-                result['error'] = e.message_dict
-            except Exception as e:
-                result['error']['reason'] = str(e)
-
-        if result['error']:
-            status = 400
-        else:
-            del result['error']
-            status = 202
+    if result['error']:
+        status = 400
+    else:
+        del result['error']
+        status = 202
 
     return HttpResponse( content=json.dumps(result, indent=4, sort_keys=True, default=str)
                          , content_type='text/json', status=status)
+
